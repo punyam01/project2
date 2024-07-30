d791eb00546f1c47449c91d42811f2bd20b6c625
punyam <punyamkumarsingh@gmail.com>
2024-07-30 22:55:53 +0530

    adding env files

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-
+require('dotenv').config()
 const express = require('express')
+
 const app = express()
-
-
 
 // declaring middlewartes for accepting vaLues
 app.use(express.json())
 
 //  declaring Routes related middlewares
 
-const movieRouters= require('./routes/movie.routes.js')
-app.use('/api/v1/movies',movieRouters);
+const movieRouters = require('./routes/movie.routes.js')
+app.use('/api/v1/movies', movieRouters)
 
-
-
-app.listen(8000, () =>{
-       console.log('server is running');
-    })
-
+app.listen(process.env.PORT, () => {
+  console.log('server is running')
+})
--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,14 +1,15 @@
 const express = require('express')
-const router= express.Router()
-const {getAllMovies,createMovie, getMovie, updateMovie, deleteMovie}= require('../controllers/movie.controller')
+const router = express.Router()
+const {
+  getAllMovies,
+  createMovie,
+  getMovie,
+  updateMovie,
+  deleteMovie
+} = require('../controllers/movie.controller')
 
-router.route('/')
-   .get(getAllMovies)
-   .post(createMovie)
+router.route('/').get(getAllMovies).post(createMovie)
 
-router.route('/:id')
-    .get(getMovie)
-    .patch(updateMovie)
-    .delete(deleteMovie)
+router.route('/:id').get(getMovie).patch(updateMovie).delete(deleteMovie)
 
-module.exports = router;
+module.exports = router
